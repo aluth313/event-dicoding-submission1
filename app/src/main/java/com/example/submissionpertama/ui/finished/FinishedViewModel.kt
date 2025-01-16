315109be0ec16cbaa567b6e4da82b46a25b55e8f
aluth313 <ahmadluthfi@deptechdigital.com>
2025-01-16 10:18:26 +0700

    finished fragment

--- app/src/main/java/com/example/submissionpertama/ui/finished/FinishedViewModel.kt
+++ app/src/main/java/com/example/submissionpertama/ui/finished/FinishedViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.submissionpertama.ui.notifications
+package com.example.submissionpertama.ui.finished
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FinishedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Finished Fragment"
     }
     val text: LiveData<String> = _text
 }

f6113e6263b418a9ef9694c63fff2de7dc25aa8b
luthfi <luthfiahmad36@gmail.com>
2025-01-18 23:48:02 +0700

    create detail event screen

---
+++ app/src/main/java/com/example/submissionpertama/ui/detailevent/DetailEventActivity.kt
@@ -0,0 +1,23 @@
+package com.example.submissionpertama.ui.detailevent
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.submissionpertama.R
+
+class DetailEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_event)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar?.hide()
+    }
+}
--- app/src/main/java/com/example/submissionpertama/ui/finished/FinishedEventAdapter.kt
+++ app/src/main/java/com/example/submissionpertama/ui/finished/FinishedEventAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.submissionpertama.ui.finished
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -8,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.submissionpertama.data.response.EventsItem
 import com.example.submissionpertama.databinding.ItemFinishedEventBinding
+import com.example.submissionpertama.ui.detailevent.DetailEventActivity
 
 class FinishedEventAdapter : ListAdapter<EventsItem, FinishedEventAdapter.MyViewHolder>(DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -27,8 +30,14 @@ class FinishedEventAdapter : ListAdapter<EventsItem, FinishedEventAdapter.MyView
             Glide.with(itemView.context)
                 .load(event.imageLogo)
                 .into(binding.ivImageFinishedEvent)
+            binding.cardFinishedEvent.setOnClickListener{ view ->
+                Log.e("RECYCLEVIEW CLICK", "ini habis di click ${event.name}")
+                view.context.startActivity(Intent(view.context, DetailEventActivity::class.java))
+            }
         }
     }
+
+
 
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<EventsItem>() {
